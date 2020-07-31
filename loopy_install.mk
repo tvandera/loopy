99f45b0254a7af1602a34e092b61d044dce59c6a
Tom Vander Aa <vanderaa@imec.be>
2020-07-31 15:27:58 +0200

    Update loopy_install.mk to use llvm git

--- loopy_install.mk
+++ loopy_install.mk
@@ -4,19 +4,16 @@
 # =========================== Customize =========================== 
 
 # Loopy root directory
-ROOT_DIR=/home/loopy
+ROOT_DIR=$(shell pwd)
 
 # Number of processors to run make on 
-PAR_BUILD=2
+PAR_BUILD=8
 
 
 # =========================== Derived =========================== 
 # LLVM root directory
-LLVM_DIR=${ROOT_DIR}/llvm
-LLVM_BUILD_DIR=${LLVM_DIR}/build
-
-# Loopy source directory
-SRC_DIR=${ROOT_DIR}/src
+LLVM_DIR=${ROOT_DIR}/llvm-project
+LLVM_BUILD_DIR=${ROOT_DIR}/build
 
 # Tests directory
 TESTS_DIR=${ROOT_DIR}/tests
@@ -27,23 +24,17 @@ TESTS_DIR=${ROOT_DIR}/tests
 all: setup get_llvm get_loopy get_clang build 
 
 setup: 
-	mkdir -p ${LLVM_DIR}
-	mkdir -p ${LLVM_DIR}/build
-
-# install LLVM, require specific version
-get_llvm: setup
-	svn co -r 241394 http://llvm.org/svn/llvm-project/llvm/trunk ${LLVM_DIR}/trunk
-
-get_clang: setup
-	 svn co -r 241394 http://llvm.org/svn/llvm-project/cfe/trunk ${LLVM_DIR}/trunk/tools/clang
-
-# copy Loopy code
-get_loopy: setup
-	cp -R ${SRC_DIR}/ ${LLVM_DIR}/trunk/tools/polly
+	mkdir -p ${LLVM_BUILD_DIR}
+	git submodule update --init
 
 # build LLVM + loopy
-build: setup get_loopy get_clang get_llvm 
-	(cd ${LLVM_BUILD_DIR}; cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=${LLVM_DIR} ${LLVM_DIR}/trunk; make -j ${PAR_BUILD})
+build: setup
+	(   cd ${LLVM_BUILD_DIR} && \
+	    cmake -G 'Unix Makefiles' ../llvm-project/llvm \
+	      -DLLVM_EXTERNAL_CLANG_SOURCE_DIR=../llvm-project/clang \
+	      -DLLVM_EXTERNAL_POLLY_SOURCE_DIR=../llvm-project/polly && \
+	    make -j ${PAR_BUILD} \
+	)
 
 
 # =========================== Test =========================== 
